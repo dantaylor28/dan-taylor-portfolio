ac8f1b2a040a2c2b76c84770f738bb04f4290b00
Dan <dan.taylor1493@gmail.com>
2023-11-30 11:54:31 +0100

    Set background colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-slate-100 text-slate-950`}>
+        <div
+          className="bg-[#ffedd5] absolute -z-10 top-[-6rem] right-[4rem] h-[31.25rem] w-[31.25rem]
+        rounded-full blur-[10rem] sm:w-[68.75rem]"
+        ></div>
+        <div
+          className="bg-[#fee2e2] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem]
+        rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        ></div>
+        <div
+          className="bg-[#f7fee7] absolute -z-10 top-[22rem] left-[-5rem] h-[31.25rem] w-[31.25rem]
+        rounded-full blur-[10rem] sm:w-[79.25rem] md:right-[-33rem] lg:right-[-28rem] xl:right-[-15rem] 2xl:right-[-5rem]"
+        ></div>
+        {children}
+      </body>
     </html>
   );
 }
