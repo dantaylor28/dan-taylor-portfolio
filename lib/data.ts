46f95cad817561564cc3a0edb4bdf8a73f718ae0
Dan <dan.taylor1493@gmail.com>
2023-11-30 12:22:27 +0100

    Define nav links data

---
+++ lib/data.ts
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    name: "Home",
+    hashtag: "#home",
+  },
+  {
+    name: "About Me",
+    hashtag: "#aboutme",
+  },
+  {
+    name: "Projects",
+    hashtag: "#projects",
+  },
+  {
+    name: "Skills",
+    hashtag: "#skills",
+  },
+  {
+    name: "Experience",
+    hashtag: "#experience",
+  },
+  {
+    name: "Contact Me",
+    hashtag: "#contactme",
+  },
+] as const;
