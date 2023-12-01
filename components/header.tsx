57d3cc95b927fbb6576e742dac3a31b01d045fab
Dan <dan.taylor1493@gmail.com>
2023-12-01 08:54:00 +0100

    Create header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { links } from "@/lib/data";
+import Link from "next/link";
+import React from "react";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="relative z-[999]">
+      <div
+        className="fixed top-0 left-1/2 h-[4.5rem] w-full
+        rounded-none border border-white border-opacity-40
+        bg-white bg-opacity-80 shadow-lg shadow-black/[0.03]
+        backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[40rem] sm:rounded-full -translate-x-1/2"
+      ></div>
+      <nav
+        className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2
+      py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0"
+      >
+        <ul
+          className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1
+        text-[0.9rem] font-medium text-gray-500 sm:w-[40rem] sm:flex-nowrap sm:gap-5"
+        >
+          {links.map((link) => (
+            <li
+              className="h-3/4 flex items-center justify-center"
+              key={link.hashtag}
+            >
+              <Link
+                href={link.hashtag}
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
