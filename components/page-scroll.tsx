2f9f96bb18e931776bf5d7c9af655250c7366f1e
Dan <dan.taylor1493@gmail.com>
2023-12-11 11:51:04 +0100

    Animate page-scroll component

--- components/page-scroll.tsx
+++ components/page-scroll.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import { motion } from "framer-motion";
 
 export default function PageScroll() {
   return (
-    <div className="hidden sm:block mt-8">
+    <motion.div
+      className="hidden sm:flex sm:justify-center sm:items-center mt-8"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.725,
+      }}
+    >
       <Link href="#aboutme">
-        <IoIosArrowDown className="text-slate-300 h-12 w-12 animate-pulse hover:text-slate-400" />
+        <IoIosArrowDown className="text-slate-400 h-12 w-12 animate-pulse hover:text-slate-500" />
       </Link>
-    </div>
+    </motion.div>
   );
 }
