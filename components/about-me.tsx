a96964c69cefc434be9519625d03e8ee59644448
Dan <dan.taylor1493@gmail.com>
2023-12-11 17:16:17 +0100

    Style about me text & add background colour

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -2,23 +2,25 @@ import React from "react";
 
 export default function AboutMe() {
   return (
-    <div>
-      <h2>About Me</h2>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos odio,
-        enim autem nihil itaque a non quia iusto cupiditate vitae fuga! Quos ad
-        ipsam consequuntur repellat nulla vitae animi quis! Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Quis labore corporis pariatur
-        aspernatur adipisci inventore, ratione recusandae deleniti, veritatis
-        consequuntur fugit provident commodi expedita ipsa voluptates odio quo
-        rem harum?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
-        expedita, consectetur explicabo corporis tempora odio beatae repudiandae
-        quos aliquid eaque magni nam voluptatum, accusamus, iure repellat
-        excepturi ab minus perferendis.
-      </p>
+    <div className="flex w-[100%] justify-center items-center bg-cyan-600 mt-10 border-t-2 border-cyan-50">
+      <section className="my-28 max-w-[50rem] text-center text-cyan-50 px-4 leading-8">
+        <h2 className="text-3xl font-medium capitalize mb-8">About Me</h2>
+        <p className="mb-3 font-light">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos odio,
+          enim autem nihil itaque a non quia iusto cupiditate vitae fuga! Quos
+          ad ipsam consequuntur repellat nulla vitae animi quis! Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Quis labore corporis
+          pariatur aspernatur adipisci inventore, ratione recusandae deleniti,
+          veritatis consequuntur fugit provident commodi expedita ipsa
+          voluptates odio quo rem harum?
+        </p>
+        <p className="font-light">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores
+          expedita, consectetur explicabo corporis tempora odio beatae
+          repudiandae quos aliquid eaque magni nam voluptatum, accusamus, iure
+          repellat excepturi ab minus perferendis.
+        </p>
+      </section>
     </div>
   );
 }
