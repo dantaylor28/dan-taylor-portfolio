f753dc83b74eae2f2ca24d35094e5b7febc71182
Dan <dan.taylor1493@gmail.com>
2023-12-21 17:04:24 +0100

    Style buttons on project section

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import PageHeading from "./page-heading";
 import Image from "next/image";
 import ProjectImg from "../public/project_img.png";
+import { BsBoxArrowUpRight, BsGithub } from "react-icons/bs";
 
 export default function Projects() {
   return (
@@ -14,23 +15,35 @@ export default function Projects() {
         </div>
 
         <div className="flex flex-col md:flex-row md:h-[35rem] overflow-hidden">
-          
-          <div className="flex items-end justify-center bg-gray-200 rounded-sm md:w-1/2">
-            <Image src={ProjectImg} alt="project I coded" quality={95}
-            className="md:w-[28.5rem] md:h-[32rem] rounded-sm" />
+          <div className="flex items-end justify-center bg-slate-200 rounded-md md:w-1/2">
+            <Image
+              src={ProjectImg}
+              alt="project I coded"
+              quality={95}
+              className="md:w-[28.5rem] md:h-[32rem] rounded-sm shadow-2xl"
+            />
           </div>
 
-          <div className="flex flex-col items-start justify-center text-left pl-[8rem] md:w-1/2">
-            <h3 className="text-3xl font-normal capitalize mb-10">project title</h3>
-            <p className="font-light">
+          <div className="flex flex-col items-start justify-center text-left pl-32 md:w-1/2">
+            <h3 className="text-3xl font-normal capitalize mb-10">
+              project title
+            </h3>
+            <p className="font-light mb-12">
               project description Lorem ipsum dolor, sit amet consectetur
               adipisicing elit. Magnam ratibutone enim qui rerum architecto quod
               reiciendis, iusto et maiores placeat ut odit mollitia. Aspernatur
               eos suscipit natus veniam? Laudantium, magnam.
             </p>
-            <div>
-              <button>repo link</button>
-              <button>live site</button>
+            <div className="flex gap-8 capitalize font-medium">
+              <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
+                live site
+                <BsBoxArrowUpRight className="group-hover:text-cyan-50 group-hover:-translate-y-1 group-hover:translate-x-1 transition" />
+              </a>
+              
+              <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
+                github link
+                <BsGithub className="group-hover:text-cyan-50 group-hover:translate-x-1 transition"/>
+              </a>
             </div>
           </div>
         </div>
