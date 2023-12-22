c2482c643802dd46ef8b3f21692119abb1deca9c
Dan <dan.taylor1493@gmail.com>
2023-12-22 09:16:17 +0100

    Final layout changes to projects section

--- components/page-heading.tsx
+++ components/page-heading.tsx
@@ -6,7 +6,7 @@ type PageHeadingProps = {
 
 export default function PageHeading({ children }: PageHeadingProps) {
   return (
-    <span className="text-[28px] sm:text-3xl px-5 capitalize font-medium text-center">
+    <span className="text-[28px] sm:text-3xl px-5 capitalize font-medium text-center m-auto">
       {children}
     </span>
   );
--- components/projects.tsx
+++ components/projects.tsx
@@ -6,43 +6,44 @@ import { BsBoxArrowUpRight, BsGithub } from "react-icons/bs";
 
 export default function Projects() {
   return (
-    <div className="flex w-[100%] justify-center items-center mt-10">
+    <div className="flex w-[100%] justify-center items-center">
       <section className="my-28 text-center px-4 leading-8 max-w-[80rem]">
         <div className="flex items-center py-4 mb-8">
-          <div className="flex-grow h-px bg-slate-950 opacity-40"></div>
+          <div className="md:hidden flex-grow h-px bg-slate-950 opacity-40"></div>
           <PageHeading>my projects</PageHeading>
-          <div className="flex-grow h-px bg-slate-950 opacity-40"></div>
+          <div className="md:hidden flex-grow h-px bg-slate-950 opacity-40"></div>
         </div>
 
         <div className="flex flex-col md:flex-row md:h-[35rem] overflow-hidden">
-          <div className="flex items-end justify-center bg-slate-200 rounded-md md:w-1/2">
+          <div className="flex items-end justify-center bg-slate-200 rounded-md md:w-1/2 md:h-[30rem] lg:h-[35rem] md:mt-10 lg:mt-0">
             <Image
               src={ProjectImg}
               alt="project I coded"
               quality={95}
-              className="md:w-[28.5rem] md:h-[32rem] rounded-sm shadow-2xl"
+              className="md:w-[20.5rem] md:h-[24rem] lg:w-[28.5rem] lg:h-[32rem] rounded-sm shadow-2xl"
             />
           </div>
 
-          <div className="flex flex-col items-start justify-center text-left pl-32 md:w-1/2">
-            <h3 className="text-3xl font-normal capitalize mb-10">
+          <div className="flex flex-col md:items-start justify-center text-left md:ml-10 lg:ml-32 md:w-1/2 mt-10 md:mt-0 md:border-y border-black/20">
+            <h3 className="text-3xl font-normal capitalize mb-10 pl-5 md:pl-0">
               project title
             </h3>
-            <p className="font-light mb-12">
+            <p className="font-extralight mb-12 pl-5 md:pl-0">
               project description Lorem ipsum dolor, sit amet consectetur
               adipisicing elit. Magnam ratibutone enim qui rerum architecto quod
               reiciendis, iusto et maiores placeat ut odit mollitia. Aspernatur
               eos suscipit natus veniam? Laudantium, magnam.
             </p>
-            <div className="flex gap-8 capitalize font-medium">
+
+            <div className="flex items-center justify-center gap-8 capitalize font-normal">
               <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
                 live site
                 <BsBoxArrowUpRight className="group-hover:text-cyan-50 group-hover:-translate-y-1 group-hover:translate-x-1 transition" />
               </a>
-              
+
               <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
                 github link
-                <BsGithub className="group-hover:text-cyan-50 group-hover:translate-x-1 transition"/>
+                <BsGithub className="group-hover:text-cyan-50 group-hover:translate-x-1 transition" />
               </a>
             </div>
           </div>
