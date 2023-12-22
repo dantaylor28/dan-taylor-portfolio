9329ab1a4fe9a7033969393d780d835ac5565817
Dan <dan.taylor1493@gmail.com>
2023-12-22 10:33:36 +0100

    Refactor project section to map over projectData

---
+++ components/project.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Image from "next/image";
+import ProjectImg from "../public/project_img.png";
+import { BsBoxArrowUpRight, BsGithub } from "react-icons/bs";
+import { projectData } from "@/lib/data";
+
+type ProjectProps = (typeof projectData)[number];
+
+export default function Project({
+  title,
+  description,
+  image,
+  tags,
+}: ProjectProps) {
+  return (
+    <div className="flex flex-col md:flex-row md:h-[35rem] overflow-hidden py-16 sm:p-0 border-b border-black/20 sm:border-none sm:mt-16">
+      <div className="flex items-end justify-center bg-slate-200 rounded-md md:w-1/2 md:h-[30rem] lg:h-[35rem] md:mt-10 lg:mt-0">
+        <Image
+          src={image}
+          alt="project I coded"
+          quality={95}
+          className="md:w-[20.5rem] md:h-[24rem] lg:w-[28.5rem] lg:h-[32rem] rounded-sm shadow-2xl"
+        />
+      </div>
+
+      <div className="flex flex-col md:items-start justify-center text-left md:ml-10 lg:ml-32 md:w-1/2 mt-10 md:mt-0 md:border-y border-black/20">
+        <h3 className="text-3xl font-normal capitalize mb-10 pl-5 md:pl-0">
+          {title}
+        </h3>
+        <p className="font-extralight mb-12 pl-5 md:pl-0">{description}</p>
+
+        <div className="flex items-center justify-center gap-8 capitalize font-normal">
+          <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
+            live site
+            <BsBoxArrowUpRight className="group-hover:text-cyan-50 group-hover:-translate-y-1 group-hover:translate-x-1 transition" />
+          </a>
+
+          <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
+            github link
+            <BsGithub className="group-hover:text-cyan-50 group-hover:translate-x-1 transition" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/projects.tsx
+++ components/projects.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import PageHeading from "./page-heading";
-import Image from "next/image";
-import ProjectImg from "../public/project_img.png";
-import { BsBoxArrowUpRight, BsGithub } from "react-icons/bs";
+import { projectData } from "@/lib/data";
+import Project from "./project";
 
 export default function Projects() {
   return (
@@ -14,40 +13,11 @@ export default function Projects() {
           <div className="md:hidden flex-grow h-px bg-slate-950 opacity-40"></div>
         </div>
 
-        <div className="flex flex-col md:flex-row md:h-[35rem] overflow-hidden">
-          <div className="flex items-end justify-center bg-slate-200 rounded-md md:w-1/2 md:h-[30rem] lg:h-[35rem] md:mt-10 lg:mt-0">
-            <Image
-              src={ProjectImg}
-              alt="project I coded"
-              quality={95}
-              className="md:w-[20.5rem] md:h-[24rem] lg:w-[28.5rem] lg:h-[32rem] rounded-sm shadow-2xl"
-            />
-          </div>
-
-          <div className="flex flex-col md:items-start justify-center text-left md:ml-10 lg:ml-32 md:w-1/2 mt-10 md:mt-0 md:border-y border-black/20">
-            <h3 className="text-3xl font-normal capitalize mb-10 pl-5 md:pl-0">
-              project title
-            </h3>
-            <p className="font-extralight mb-12 pl-5 md:pl-0">
-              project description Lorem ipsum dolor, sit amet consectetur
-              adipisicing elit. Magnam ratibutone enim qui rerum architecto quod
-              reiciendis, iusto et maiores placeat ut odit mollitia. Aspernatur
-              eos suscipit natus veniam? Laudantium, magnam.
-            </p>
-
-            <div className="flex items-center justify-center gap-8 capitalize font-normal">
-              <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
-                live site
-                <BsBoxArrowUpRight className="group-hover:text-cyan-50 group-hover:-translate-y-1 group-hover:translate-x-1 transition" />
-              </a>
-
-              <a className="group bg-cyan-50 text-cyan-800 flex items-center gap-2 hover:bg-cyan-600 hover:text-cyan-50 duration-300 border border-black/10 py-2 px-6 rounded cursor-pointer">
-                github link
-                <BsGithub className="group-hover:text-cyan-50 group-hover:translate-x-1 transition" />
-              </a>
-            </div>
-          </div>
-        </div>
+        {projectData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
       </section>
     </div>
   );
