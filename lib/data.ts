e7e3073a0da7d05eda31cfc2895c8970c4d12999
Dan <dan.taylor1493@gmail.com>
2023-12-22 09:55:07 +0100

    Add initial project data to data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,3 +1,7 @@
+import ProjectImg from "../public/project_img.png";
+import DrfApi from "../public/drf_api.png"
+import FoodBlog from "../public/food_blog.png"
+
 export const links = [
   {
     name: "Home",
@@ -22,5 +26,29 @@ export const links = [
   {
     name: "Contact Me",
     hashtag: "#contactme",
+  },
+] as const;
+
+export const projectData = [
+  {
+    title: "Polaroid",
+    description:
+      "Polaroid is a social media CRUD application developed using React.js, where users can share images and text from their everyday lives",
+    image: ProjectImg,
+    tags: ["ReactJS", "Bootstrap", "CSS", "Axios"],
+  },
+  {
+    title: "DRF API",
+    description:
+      "A social media API, where users can create, read, update and delete data. This project was developed using the Django Rest Framework",
+    image: DrfApi,
+    tags: ["Python", "DRF", "API", "CRUD"],
+  },
+  {
+    title: "The Food Blog",
+    description:
+      "The Food Blog is a Django full stack application where users can discover & share recipes and other general culinary blogs",
+    image: FoodBlog,
+    tags: ["Python", "Django", "Bootstrap", "HTML", "Cloudinary"],
   },
 ] as const;
