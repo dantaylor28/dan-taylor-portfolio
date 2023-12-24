4bfdc254073074236a679b9fb4087c583d856517
Dan <dan.taylor1493@gmail.com>
2023-12-24 08:40:51 +0100

    Add even/odd class to swap project cards

--- components/project.tsx
+++ components/project.tsx
@@ -13,17 +13,17 @@ export default function Project({
   tags,
 }: ProjectProps) {
   return (
-    <div className="flex flex-col md:flex-row md:h-[35rem] overflow-hidden py-16 sm:p-0 border-b border-black/20 sm:border-none sm:mt-16">
-      <div className="flex items-end justify-center bg-slate-200 rounded-md group md:w-1/2 md:h-[30rem] lg:h-[35rem] md:mt-10 lg:mt-0 hover:bg-slate-300 transition">
+    <div className="group/margin flex flex-col md:flex-row md:h-[35rem] overflow-hidden py-16 sm:p-0 border-b border-black/20 sm:border-none sm:mt-16 sm:mb-32 md:odd:flex-row-reverse ">
+      <div className="group flex items-end justify-center bg-slate-200 rounded-md group md:w-1/2 md:h-[30rem] lg:h-[35rem] md:mt-10 lg:mt-0 hover:bg-slate-300 transition">
         <Image
           src={image}
           alt="project I coded"
           quality={95}
-          className="md:w-[20.5rem] md:h-[24rem] lg:w-[28.5rem] lg:h-[32rem] rounded-sm shadow-2xl group-hover:scale-105"
+          className="md:w-[20.5rem] md:h-[24rem] lg:w-[28.5rem] lg:h-[32rem] rounded-sm shadow-2xl group-hover:scale-105 "
         />
       </div>
-
-      <div className="flex flex-col md:items-start justify-center text-left md:ml-10 lg:ml-32 md:w-1/2 mt-10 md:mt-0 md:border-y border-black/20">
+    
+      <div className="flex flex-col md:items-start justify-center text-left md:ml-10 lg:ml-32 md:w-1/2 mt-10 md:mt-0 md:border-y border-black/20 group-odd/margin:ml-0 md:group-odd/margin:mr-10 lg:group-odd/margin:mr-32">
         <h3 className="text-3xl font-normal capitalize mb-10 pl-5 md:pl-0">
           {title}
         </h3>
