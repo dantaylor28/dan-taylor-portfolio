f826916ffc64a171e74cde550dcffeeba241f4db
Dan <dan.taylor1493@gmail.com>
2024-01-03 17:42:03 +0100

    Define useActiveSectionContext hook

--- contexts/active-section-context.tsx
+++ contexts/active-section-context.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { links } from "@/lib/data";
-import React, { useState } from "react";
+import React, { useState, createContext, useContext } from "react";
 
 type SectionName = (typeof links)[number]["name"];
 
@@ -7,9 +9,39 @@ type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
 };
 
+type ActiveSectionContextType = {
+  activeSection: SectionName;
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+};
+
+const ActiveSectionContext = createContext<ActiveSectionContextType | null>(
+  null
+);
+
 export default function ActiveSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
-  return children;
+  return (
+    <ActiveSectionContext.Provider
+      value={{
+        activeSection,
+        setActiveSection,
+      }}
+    >
+      {children}
+    </ActiveSectionContext.Provider>
+  );
+}
+
+export function useActiveSectionContext() {
+  const context = useContext(ActiveSectionContext);
+
+  if (context === null) {
+    throw new Error(
+      "useActiveSectionContext must be used within an ActiveSectionContextProvider"
+    );
+  }
+
+  return context;
 }
