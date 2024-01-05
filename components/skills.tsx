479a27a4e68cd32aa9be0b7f3bc61804a2171249
Dan <dan.taylor1493@gmail.com>
2024-01-05 19:14:08 +0100

    Add icons to the skills section

--- components/skills.tsx
+++ components/skills.tsx
@@ -3,8 +3,92 @@ import PageHeading from "./page-heading";
 
 export default function Skills() {
   return (
-    <section>
+    <section
+      id="skills"
+      className="text-center scroll-mt-28 max-w-[60rem] mb-28"
+    >
       <PageHeading>my skills</PageHeading>
+      <div className="flex flex-wrap gap-8 mt-20 justify-center">
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--react] text-[3.5rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--nextjs-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--html-5] text-[3.5rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--css-3] text-[3.5rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--python] text-[3.5rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--docker-icon] text-[2.9rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--django-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--bootstrap] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--nodejs-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--typescript-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--tailwindcss-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--git-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--postgresql] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--mongodb-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--mysql-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--visual-studio-code] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--heroku-icon] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--prettier] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--eslint] text-[3rem]"></span>
+        </div>
+
+        <div className="flex items-center justify-center h-20 w-20 border border-black/10 rounded-full">
+          <span className="icon-[logos--xcode] text-[3rem]"></span>
+        </div>
+      </div>
     </section>
   );
 }
