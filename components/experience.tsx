716ab59c465d05140afdb69439926266f0ed61ce
Dan <dan.taylor1493@gmail.com>
2024-01-18 08:43:51 +0100

    Refactor Experience component into 2 seperate ones

--- components/experience.tsx
+++ components/experience.tsx
@@ -11,27 +11,39 @@ import PageHeading from "./page-heading";
 import { useSectionInView } from "@/lib/hooks";
 import { useInView } from "react-intersection-observer";
 
-export default function Experience() {
+type ExperienceElementProps = {
+  item: {
+    title: string;
+    location: string;
+    description: string;
+  };
+};
+
+const ExperienceElement = ({ item }: ExperienceElementProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.75,
+  });
+  return (
+    <div ref={ref} className="vertical-timeline-element">
+      <VerticalTimelineElement visible={inView}>
+        <h1>{item.title}</h1>
+        <p>{item.location}</p>
+        <p>{item.description}</p>
+      </VerticalTimelineElement>
+    </div>
+  );
+};
+
+export default function experience() {
   const { ref } = useSectionInView("Experience", 0.2);
   return (
     <section id="experience" ref={ref}>
       <PageHeading>my experience</PageHeading>
       <VerticalTimeline>
-        {experienceData.map((item, index) => {
-          const { ref, inView } = useInView({
-            triggerOnce: true,
-            threshold: 0.75,
-          });
-          return (
-            <div key={index} ref={ref} className="vertical-timeline-element">
-              <VerticalTimelineElement visible={inView}>
-                <h1>{item.title}</h1>
-                <p>{item.location}</p>
-                <p>{item.description}</p>
-              </VerticalTimelineElement>
-            </div>
-          );
-        })}
+        {experienceData.map((item, index) => (
+          <ExperienceElement key={index} item={item} />
+        ))}
       </VerticalTimeline>
     </section>
   );
