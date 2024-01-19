af66ce8f129c0717b4c94cc8249527286611859e
Dan <dan.taylor1493@gmail.com>
2024-01-19 19:01:04 +0100

    Create skeleton form for contact page

--- components/contact-me.tsx
+++ components/contact-me.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import PageHeading from "./page-heading";
 
 export default function ContactMe() {
-  return <section id="contactme">
-    <PageHeading>contact me</PageHeading>
-  </section>;
+  return (
+    <section id="contactme" className="min-w-[30rem] mb-28">
+      <PageHeading>contact me</PageHeading>
+
+      <form action="" className="mt-10 flex flex-col">
+        <input type="name" className="mb-5" placeholder="Name"/>
+        <input type="email" className="mb-5" placeholder="Email"/>
+        <textarea className="mb-5" placeholder="Message"/>
+        <button type="submit">
+            Send Message
+        </button>
+      </form>
+    </section>
+  );
 }
