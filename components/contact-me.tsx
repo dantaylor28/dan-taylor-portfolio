bc765f846573486a9b4a06cefbfa09dd7b0f520b
Dan <dan.taylor1493@gmail.com>
2024-01-20 21:33:45 +0100

    Attach ref and useSectionInView hook in the form

--- components/contact-me.tsx
+++ components/contact-me.tsx
@@ -1,18 +1,24 @@
+"use client";
+
 import React from "react";
 import PageHeading from "./page-heading";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function ContactMe() {
+  const { ref } = useSectionInView("Contact Me", 0.5);
   return (
-    <section id="contactme" className="min-w-[30rem] mb-28">
+    <section
+      id="contactme"
+      className="min-w-[30rem] mb-28 h-[50rem]"
+      ref={ref}
+    >
       <PageHeading>contact me</PageHeading>
 
       <form action="" className="mt-10 flex flex-col">
-        <input type="name" className="mb-5" placeholder="Name"/>
-        <input type="email" className="mb-5" placeholder="Email"/>
-        <textarea className="mb-5" placeholder="Message"/>
-        <button type="submit">
-            Send Message
-        </button>
+        <input type="name" className="mb-5" placeholder="Name" />
+        <input type="email" className="mb-5" placeholder="Email" />
+        <textarea className="mb-5" placeholder="Message" />
+        <button type="submit">Send Message</button>
       </form>
     </section>
   );
