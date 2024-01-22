83f5ddd78ba2acac48d972e230b183016490f73d
Dan <dan.taylor1493@gmail.com>
2024-01-22 10:41:54 +0100

    Create & implement a stringValidation function

--- actions/sendEmailData.ts
+++ actions/sendEmailData.ts
@@ -4,16 +4,34 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+const validateString = (value: unknown) => {
+  if (!value || typeof value !== "string") {
+    return false;
+  }
+  return true;
+};
+
 export const sendEmailData = async (formData: FormData) => {
   const name = formData.get("name");
   const email = formData.get("email");
   const message = formData.get("message");
+
+  if (!validateString(email)) {
+    return {
+      error: "This email is invalid",
+    };
+  }
+  if (!validateString(message)) {
+    return {
+      error: "This message is invalid",
+    };
+  }
 
   resend.emails.send({
     from: "onboarding@resend.dev",
     to: "dan.taylor1493@gmail.com",
     subject: "Message from portfolio site",
     text: message,
-    reply_to: email
+    reply_to: email,
   });
 };
