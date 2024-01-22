3b4ebdec9e245181f791499c8322ae0bc69760a3
Dan <dan.taylor1493@gmail.com>
2024-01-22 10:09:52 +0100

    Hook up resend to send hardcoded emails

--- actions/sendEmailData.ts
+++ actions/sendEmailData.ts
@@ -1,8 +1,19 @@
 "use server";
 
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 export const sendEmailData = async (formData: FormData) => {
   console.log("Running on server");
   console.log(formData.get("name"));
   console.log(formData.get("email"));
   console.log(formData.get("message"));
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "dan.taylor1493@gmail.com",
+    subject: "Message from portfolio site",
+    text: "Hello, world!!!!!",
+  });
 };
