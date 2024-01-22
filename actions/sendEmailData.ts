6eacd86353165526fa5887d25683feacdff61e92
Dan <dan.taylor1493@gmail.com>
2024-01-22 10:21:20 +0100

    Feed resend email with dynamic form data

--- actions/sendEmailData.ts
+++ actions/sendEmailData.ts
@@ -5,15 +5,15 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmailData = async (formData: FormData) => {
-  console.log("Running on server");
-  console.log(formData.get("name"));
-  console.log(formData.get("email"));
-  console.log(formData.get("message"));
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const message = formData.get("message");
 
   resend.emails.send({
     from: "onboarding@resend.dev",
     to: "dan.taylor1493@gmail.com",
     subject: "Message from portfolio site",
-    text: "Hello, world!!!!!",
+    text: message,
+    reply_to: email
   });
 };
