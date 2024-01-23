e98aba0568efb20f576483361902605c3bc90f38
Dan <dan.taylor1493@gmail.com>
2024-01-23 11:19:36 +0100

    Create boilerplate react email in emailForm.tsx

--- email/emailForm.tsx
+++ email/emailForm.tsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  Html,
+  Body,
+  Head,
+  Heading,
+  Hr,
+  Container,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
 
-export default function EmailForm() {
+import { Tailwind } from "@react-email/tailwind";
+
+type EmailFormProps = {
+  name: string;
+  message: string;
+  email: string;
+};
+
+export default function EmailForm({ name, message, email }: EmailFormProps) {
   return (
-    <div>EmailForm</div>
-  )
+    <Html>
+      <Head>
+        <title>Message from portfolio site</title>
+      </Head>
+      <Preview>New message from your contact form</Preview>
+      <Tailwind>
+        <Body>
+          <Container>
+            <Section>
+              <Heading>You received the following message:</Heading>
+              <Text>{message}</Text>
+              <Hr />
+              <Text>Message recieved from: {name}</Text>
+              <Text>Email Address: {email}</Text>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
 }
