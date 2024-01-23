fc5e784287ad5780ee9975f84306839e9fe49bbb
Dan <dan.taylor1493@gmail.com>
2024-01-23 08:29:22 +0100

    Refactor validateString into reusable utils file

--- actions/sendEmailData.ts
+++ actions/sendEmailData.ts
@@ -1,15 +1,9 @@
 "use server";
 
 import { Resend } from "resend";
+import { validateString } from "@/lib/utilities";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-
-const validateString = (value: unknown, maxLength: number) => {
-  if (!value || typeof value !== "string" || value.length > maxLength) {
-    return false;
-  }
-  return true;
-};
 
 export const sendEmailData = async (formData: FormData) => {
   const name = formData.get("name");
