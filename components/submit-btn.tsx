00a2a04dde403e84370b169515a13335831511b3
Dan <dan.taylor1493@gmail.com>
2024-01-26 16:49:00 +0100

    Add a loading spinner while form is pending

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -5,14 +5,21 @@ import { PiPaperPlaneTilt } from "react-icons/pi";
 import { useFormStatus } from "react-dom";
 
 export default function SubmitBtn() {
-    const { pending } = useFormStatus();
+  const { pending } = useFormStatus();
   return (
     <button
       type="submit"
-      className="group bg-cyan-50 text-cyan-800 px-5 py-3 rounded-full flex items-center gap-2 hover:bg-cyan-500 hover:text-white transition duration-300 hover:scale-105"
+      className="group bg-cyan-50 text-cyan-800 px-5 py-3 rounded-full flex items-center gap-2 hover:bg-cyan-500 hover:text-white transition duration-300 hover:scale-105 disabled:scale-100 disabled:bg-cyan-500"
+      disabled={pending}
     >
-      Send
-      <PiPaperPlaneTilt className="group-hover:text-white group-hover:translate-x-1 group-hover:-translate-y-1 transition duration-300" />
+      {pending ? (
+        <div className="animate-spin h-5 w-5 rounded-full border-b-2 border-white"></div>
+      ) : (
+        <>
+          Send
+          <PiPaperPlaneTilt className="group-hover:text-white group-hover:translate-x-1 group-hover:-translate-y-1 transition duration-300" />
+        </>
+      )}
     </button>
   );
 }
