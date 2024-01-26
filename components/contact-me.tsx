5539a28e8c9b4ff0c16ac34472df6877e9da9e4b
Dan <dan.taylor1493@gmail.com>
2024-01-26 10:39:15 +0100

    Style the submit button on the contact form

--- components/contact-me.tsx
+++ components/contact-me.tsx
@@ -5,6 +5,7 @@ import PageHeading from "./page-heading";
 import { useSectionInView } from "@/lib/hooks";
 import { sendEmailData } from "@/actions/sendEmailData";
 import toast from "react-hot-toast";
+import { PiPaperPlaneTilt } from "react-icons/pi";
 
 export default function ContactMe() {
   const { ref } = useSectionInView("Contact Me", 0.5);
@@ -74,7 +75,10 @@ export default function ContactMe() {
             dan.taylor1493@gmail.com
           </span>
         </p>
-        <form action="" className="mt-10">
+        <form
+          action=""
+          className="flex flex-col items-center justify-center mt-10"
+        >
           <div className="flex items-center justify-center gap-10">
             <input
               type="name"
@@ -96,10 +100,17 @@ export default function ContactMe() {
           <textarea
             name="message"
             className="my-10 h-52 w-[42.5rem] rounded-md py-4 px-4 bg-cyan-700 text-cyan-50 outline-none focus:outline-white/30"
-            placeholder="Message"
+            placeholder="Ask Me Anything"
             required
             maxLength={5000}
           />
+          <button
+            type="submit"
+            className="group bg-cyan-50 text-cyan-800 px-5 py-3 rounded-full flex items-center gap-2 hover:bg-cyan-500 hover:text-white transition duration-300 hover:scale-105"
+          >
+            Send
+            <PiPaperPlaneTilt className="group-hover:text-white group-hover:translate-x-1 group-hover:-translate-y-1 transition duration-300"/>
+          </button>
         </form>
       </section>
     </div>
