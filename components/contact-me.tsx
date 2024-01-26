5658b0193fd708e3cd0317ce65269bfd409b5e8d
Dan <dan.taylor1493@gmail.com>
2024-01-26 11:59:03 +0100

    Add the form action & ensure it works as expected

--- components/contact-me.tsx
+++ components/contact-me.tsx
@@ -10,55 +10,6 @@ import { PiPaperPlaneTilt } from "react-icons/pi";
 export default function ContactMe() {
   const { ref } = useSectionInView("Contact Me", 0.5);
   return (
-    // <section
-    //   id="contactme"
-    //   className="flex flex-col items-center justify-center scroll-mt-28 text-center w-full bg-slate-200"
-    //   ref={ref}
-    // >
-    //   <div className="my-28">
-    //     <PageHeading>contact me</PageHeading>
-    //     <div className="flex items-center justify-center w-[60rem] bg-black/10 mt-12 px-20 py-10">
-    //       <form
-    //         action={async (formData) => {
-    //           const { data, error } = await sendEmailData(formData);
-
-    //           if (error) {
-    //             toast.error(error);
-    //             return;
-    //           }
-    //           toast.success("Message sent successfully!");
-    //         }}
-    //         className="flex flex-col w-[30rem]"
-    //       >
-    //         <input
-    //           type="name"
-    //           name="name"
-    //           className="h-14 px-4 rounded-lg"
-    //           placeholder="Name"
-    //           required
-    //           maxLength={300}
-    //         />
-    //         <input
-    //           type="email"
-    //           name="email"
-    //           className="my-3"
-    //           placeholder="Email"
-    //           required
-    //           maxLength={500}
-    //         />
-    //         <textarea
-    //           name="message"
-    //           className=""
-    //           placeholder="Message"
-    //           required
-    //           maxLength={5000}
-    //         />
-    //         <button type="submit">Send Message</button>
-    //       </form>
-    //     </div>
-    //   </div>
-    // </section>
-
     <div
       id="contactme"
       ref={ref}
@@ -68,11 +19,24 @@ export default function ContactMe() {
         <PageHeading>get in touch</PageHeading>
         <p className="mt-5">
           You can reach me directly at{" "}
-          <a href="mailto:dan.taylor1493@gmail.com" className="underline font-semibold">dan.taylor1493@gmail.com</a>{" "}
+          <a
+            href="mailto:dan.taylor1493@gmail.com"
+            className="underline font-semibold"
+          >
+            dan.taylor1493@gmail.com
+          </a>{" "}
           or through the following form.
         </p>
         <form
-          action=""
+          action={async (formData) => {
+            const { data, error } = await sendEmailData(formData);
+
+            if (error) {
+              toast.error(error);
+              return;
+            }
+            toast.success("Message sent successfully!");
+          }}
           className="flex flex-col items-center justify-center mt-10 sm:mt-16"
         >
           <div className="flex items-center justify-center gap-10 flex-col md:flex-row">
