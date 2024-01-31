e2f0956a9880402612a929136f368f800cde8156
Dan <dan.taylor1493@gmail.com>
2024-01-31 21:35:42 +0100

    Add dark styles to the form submit button

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -9,7 +9,7 @@ export default function SubmitBtn() {
   return (
     <button
       type="submit"
-      className="group bg-cyan-50 text-cyan-800 px-5 py-3 rounded-full flex items-center gap-2 hover:bg-cyan-500 hover:text-white transition duration-300 hover:scale-105 disabled:scale-100 disabled:bg-cyan-500"
+      className="group bg-cyan-50 text-cyan-800 px-5 py-3 rounded-full flex items-center gap-2 hover:bg-cyan-500 hover:text-white transition duration-300 hover:scale-105 disabled:scale-100 disabled:bg-cyan-500 dark:bg-cyan-100/90 dark:hover:bg-cyan-800 dark:hover:border dark:hover:border-white/10"
       disabled={pending}
     >
       {pending ? (
