8b6704600245a4f27e7d2ab5c25a3ce6d07f0d65
Dan <dan.taylor1493@gmail.com>
2024-02-26 11:03:10 +0100

    Create logo.tsx file & render to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import AboutMe from "@/components/about-me";
 import ContactMe from "@/components/contact-me";
 import Experience from "@/components/experience";
 import Introduction from "@/components/introduction";
+import Logo from "@/components/logo";
 import Projects from "@/components/projects";
 import ScrollToTop from "@/components/scroll-to-top";
 import Skills from "@/components/skills";
@@ -9,6 +10,7 @@ import Skills from "@/components/skills";
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
+      <Logo />
       <Introduction />
       <AboutMe />
       <Projects />
