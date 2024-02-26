a0124a1bda21c6e11dcea7f3421958743aceb777
Dan <dan.taylor1493@gmail.com>
2024-02-26 11:17:01 +0100

    Change positioning and z index of logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Logo() {
-  return <div className="hidden lg:inline-block fixed top-8 left-8 text-2xl font-mono">
+  return <div className="hidden lg:inline-block absolute top-8 left-8 text-2xl font-mono z-[999]">
     Dan Taylor
   </div>;
 }
