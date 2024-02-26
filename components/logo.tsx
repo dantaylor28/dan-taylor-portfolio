f0dd9344a88fadc56a0bf37e648395092bcafc68
Dan <dan.taylor1493@gmail.com>
2024-02-26 13:11:54 +0100

    Animate logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,27 @@
+"use client";
+
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Logo() {
-  return <div className="hidden lg:inline-block absolute top-8 left-8 text-2xl font-mono z-[999]">
-    Dan Taylor
-  </div>;
+  return (
+    <motion.div
+      initial={{
+        opacity: 0,
+        x: -500,
+        scale: 0.5,
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+        scale: 1,
+      }}
+      transition={{
+        duration: 1.5,
+      }}
+      className="hidden lg:inline-block absolute top-8 left-8 text-2xl font-mono z-[999]"
+    >
+      Dan Taylor
+    </motion.div>
+  );
 }
