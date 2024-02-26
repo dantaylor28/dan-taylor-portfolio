e349961de252c0e85b1a5fdcb50461015d4ebdc8
Dan <dan.taylor1493@gmail.com>
2024-02-26 11:12:01 +0100

    Create placeholder logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 export default function Logo() {
-  return (
-    <div>Logo</div>
-  )
+  return <div className="hidden lg:inline-block fixed top-8 left-8 text-2xl font-mono">
+    Dan Taylor
+  </div>;
 }
