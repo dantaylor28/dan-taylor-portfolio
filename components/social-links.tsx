e31eea90e033f526dba4fe0c43c728a051bf5c32
Dan <dan.taylor1493@gmail.com>
2024-03-12 15:51:25 +0100

    Add basic layout for social links component

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { FaLinkedin, FaSquareGithub, FaInstagram } from "react-icons/fa6";
 
 export default function SocialLinks() {
   return (
-    <div>SocialLinks</div>
-  )
+    <div>
+      <a href="https://github.com/dantaylor28" target="_blank">
+        <FaSquareGithub />
+      </a>
+      <a href="https://www.linkedin.com/in/dan-m-taylor/">
+        <FaLinkedin />
+      </a>
+      <a href="https://www.instagram.com/dantaylor93_/">
+        <FaInstagram />
+      </a>
+    </div>
+  );
 }
