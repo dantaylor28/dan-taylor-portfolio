4ca8b8cee175d00224c973453ec8b6718dfad5f2
Dan <dan.taylor1493@gmail.com>
2024-03-14 10:10:41 +0100

    Animate the social links

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,34 +1,55 @@
+"use client";
+
 import React from "react";
 import { FaLinkedin, FaSquareGithub, FaInstagram } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 export default function SocialLinks() {
   return (
-    <div
+    <motion.div
+      initial={{ y: -100, x: "-50%", opacity: 0 }}
+      animate={{ y: 0, x: "-50%", opacity: 1 }}
+      transition={{
+        type: "tween",
+        delay: 0.5,
+        duration: 1,
+      }}
       className="fixed top-6 right-8 h-[3.25rem] w-[10rem] rounded-full border border-white border-opacity-40
-    bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] flex items-center justify-center
-    z-[999] text-[28px] gap-4 text-cyan-800 dark:bg-slate-950 dark:bg-opacity-80 dark:border-white/20 dark:text-cyan-50/80"
+    bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] flex items-center justify-center 
+    z-[999] text-[28px] text-cyan-800 dark:bg-slate-950 dark:bg-opacity-80 dark:border-white/20 dark:text-cyan-50/80"
     >
-      <a
-        href="https://github.com/dantaylor28"
-        target="_blank"
-        className="hover:text-cyan-700 dark:hover:text-white transition"
+      <motion.div
+      className="flex items-center justify-center gap-4"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          type: "tween",
+          delay: 1.3,
+          duration: 0.4,
+        }}
       >
-        <FaSquareGithub />
-      </a>
-      <a
-        href="https://www.linkedin.com/in/dan-m-taylor/"
-        target="_blank"
-        className="hover:text-cyan-700 dark:hover:text-white transition"
-      >
-        <FaLinkedin />
-      </a>
-      <a
-        href="https://www.instagram.com/dantaylor93_/"
-        target="_blank"
-        className="hover:text-cyan-700 dark:hover:text-white transition"
-      >
-        <FaInstagram />
-      </a>
-    </div>
+        <a
+          href="https://github.com/dantaylor28"
+          target="_blank"
+          className="hover:text-cyan-700 dark:hover:text-white transition"
+        >
+          <FaSquareGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/dan-m-taylor/"
+          target="_blank"
+          className="hover:text-cyan-700 dark:hover:text-white transition"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://www.instagram.com/dantaylor93_/"
+          target="_blank"
+          className="hover:text-cyan-700 dark:hover:text-white transition"
+        >
+          <FaInstagram />
+        </a>
+      </motion.div>
+    </motion.div>
   );
 }
