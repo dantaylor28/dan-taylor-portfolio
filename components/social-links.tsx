e24bc520e396320bf99ab9b30bae4803b8828389
Dan <dan.taylor1493@gmail.com>
2024-03-14 09:25:32 +0100

    Add hover effects to social links

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -6,15 +6,15 @@ export default function SocialLinks() {
     <div
       className="fixed top-6 right-8 h-[3.25rem] w-[10rem] rounded-full border border-white border-opacity-40
     bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] flex items-center justify-center
-    z-[999] text-[28px] gap-4 text-cyan-800 opacity-80"
+    z-[999] text-[28px] gap-4 text-cyan-800"
     >
-      <a href="https://github.com/dantaylor28" target="_blank">
+      <a href="https://github.com/dantaylor28" target="_blank" className="hover:text-cyan-700 transition">
         <FaSquareGithub />
       </a>
-      <a href="https://www.linkedin.com/in/dan-m-taylor/" target="_blank">
+      <a href="https://www.linkedin.com/in/dan-m-taylor/" target="_blank" className="hover:text-cyan-700 transition">
         <FaLinkedin />
       </a>
-      <a href="https://www.instagram.com/dantaylor93_/" target="_blank">
+      <a href="https://www.instagram.com/dantaylor93_/" target="_blank" className="hover:text-cyan-700 transition">
         <FaInstagram />
       </a>
     </div>
