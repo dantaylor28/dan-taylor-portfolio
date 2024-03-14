30008fe2f0b14eaab9ab0ad0f3f4d67b2f404fff
Dan <dan.taylor1493@gmail.com>
2024-03-14 09:47:19 +0100

    Add dark styling to social links & match it to header styles

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -6,15 +6,27 @@ export default function SocialLinks() {
     <div
       className="fixed top-6 right-8 h-[3.25rem] w-[10rem] rounded-full border border-white border-opacity-40
     bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] flex items-center justify-center
-    z-[999] text-[28px] gap-4 text-cyan-800"
+    z-[999] text-[28px] gap-4 text-cyan-800 dark:bg-slate-950 dark:bg-opacity-80 dark:border-white/20 dark:text-cyan-50/80"
     >
-      <a href="https://github.com/dantaylor28" target="_blank" className="hover:text-cyan-700 transition">
+      <a
+        href="https://github.com/dantaylor28"
+        target="_blank"
+        className="hover:text-cyan-700 dark:hover:text-white transition"
+      >
         <FaSquareGithub />
       </a>
-      <a href="https://www.linkedin.com/in/dan-m-taylor/" target="_blank" className="hover:text-cyan-700 transition">
+      <a
+        href="https://www.linkedin.com/in/dan-m-taylor/"
+        target="_blank"
+        className="hover:text-cyan-700 dark:hover:text-white transition"
+      >
         <FaLinkedin />
       </a>
-      <a href="https://www.instagram.com/dantaylor93_/" target="_blank" className="hover:text-cyan-700 transition">
+      <a
+        href="https://www.instagram.com/dantaylor93_/"
+        target="_blank"
+        className="hover:text-cyan-700 dark:hover:text-white transition"
+      >
         <FaInstagram />
       </a>
     </div>
