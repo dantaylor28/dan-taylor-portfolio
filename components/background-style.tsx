bc83eade75c70094ff40c66bb6023f07583ee532
Dan <dan.taylor1493@gmail.com>
2024-03-21 09:21:14 +0100

    Adjust colors and opacity of background styles

--- components/background-style.tsx
+++ components/background-style.tsx
@@ -19,11 +19,11 @@ function BackgroundStyle() {
       }}
       className="relative top-52 sm:top-40 flex justify-center items-center"
     >
-      <div className="absolute border border-cyan-900/20 dark:border-[#334155] h-[150px] w-[150px] sm:h-[200px] sm:w-[200px] rounded-full mt-52 animate-ping" />
-      <div className="absolute border border-slate-700/5 dark:border-[#0f172a] h-[200px] w-[200px] sm:h-[300px] sm:w-[300px] rounded-full mt-52" />
-      <div className="absolute border border-slate-700/5 dark:border-[#0f172a] h-[300px] w-[300px] sm:h-[500px] sm:w-[500px] rounded-full mt-52" />
-      <div className="absolute border border-cyan-900/20 dark:border-[#334155] dark:opacity-5 h-[400px] w-[400px] sm:h-[650px] sm:w-[650px] rounded-full mt-52 animate-pulse" />
-      <div className="absolute border border-slate-700/5 dark:border-[#0f172a] h-[500px] w-[500px] sm:h-[800px] sm:w-[800px] rounded-full mt-52" />
+      <div className="absolute border border-cyan-800 opacity-[0.2] dark:opacity-[0.3] dark:border-[#334155] h-[150px] w-[150px] sm:h-[200px] sm:w-[200px] rounded-full mt-52 animate-ping" />
+      <div className="absolute border border-slate-700/10 opacity-[0.5] dark:border-white/5 h-[200px] w-[200px] sm:h-[300px] sm:w-[300px] rounded-full mt-52" />
+      <div className="absolute border border-slate-700/10 opacity-[0.5] dark:border-white/5 h-[300px] w-[300px] sm:h-[500px] sm:w-[500px] rounded-full mt-52" />
+      <div className="absolute border border-cyan-800/[0.25] dark:border-[#ecfeff]/[0.15] h-[400px] w-[400px] sm:h-[650px] sm:w-[650px] rounded-full mt-52 animate-pulse" />
+      <div className="absolute border border-slate-700/10 opacity-[0.5] dark:border-white/5 h-[500px] w-[500px] sm:h-[800px] sm:w-[800px] rounded-full mt-52" />
     </motion.div>
   );
 }
