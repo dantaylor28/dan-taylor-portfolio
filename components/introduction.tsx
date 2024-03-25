6b2bf6f418195ef04d1bb5ebe5f7d771222ef41a
Dan <dan.taylor1493@gmail.com>
2024-03-25 12:26:10 +0100

    Restyle the typewriter text

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -76,7 +76,7 @@ export default function Introduction() {
         </motion.h2>
 
         <motion.h1
-          className="mt-8 mb-5 px-4 text-[28px] sm:text-[36px] dark:text-cyan-800 z-10"
+          className="mt-8 mb-5 px-4 text-[28px] sm:text-[34px] text-cyan-800 dark:text-cyan-50 z-10"
           initial={{ opacity: 0, y: 200 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{
@@ -84,12 +84,8 @@ export default function Introduction() {
             duration: 0.3,
           }}
         >
-          {/* Hi, I&apos;m{" "}
-          <span className="text-cyan-800 dark:text-cyan-50">{text}</span>
-          <Cursor cursorStyle="_" cursorColor="#ecfeff" /> */}
-
-          <span>{text}</span>
-          <Cursor />
+          <span className="font-serif dark:opacity-90">{text}</span>
+          <Cursor cursorStyle="_" />
         </motion.h1>
 
         <motion.div
