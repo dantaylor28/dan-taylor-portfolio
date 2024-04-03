3eae5912b958c04a605d366ba720a13b166da041
Dan <dan.taylor1493@gmail.com>
2024-04-03 10:44:51 +0200

    Implement ActiveSection & LastHeaderClick to the scrollToTop btn

--- components/scroll-to-top.tsx
+++ components/scroll-to-top.tsx
@@ -3,17 +3,23 @@
 import { useScroll } from "@/contexts/scroll-to-top-context";
 import React from "react";
 import { FaArrowUp } from "react-icons/fa6";
+import { useActiveSectionContext } from "@/contexts/active-section-context";
 
 export default function ScrollToTop() {
   const { isVisible, scrollUp } = useScroll();
+  const { setActiveSection, setLastHeaderClick } = useActiveSectionContext();
   return (
     <button
       className={`fixed bottom-5 right-8 text-white hover:animate-pulse z-[100] ${
         isVisible ? "opacity-1" : "hidden cursor-none"
       }`}
-      onClick={scrollUp}
+      onClick={() => {
+        scrollUp();
+        setActiveSection("Home");
+        setLastHeaderClick(Date.now());
+      }}
     >
-      <FaArrowUp className="h-9 w-9 rounded-full p-2 bg-gradient-to-r from-cyan-500 to-cyan-700"/>
+      <FaArrowUp className="h-9 w-9 rounded-full p-2 bg-gradient-to-r from-cyan-500 to-cyan-700" />
     </button>
   );
 }
