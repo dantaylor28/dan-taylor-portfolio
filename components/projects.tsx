39efaf69f669f6699081d449505b40ee03e99114
Dan <dan.taylor1493@gmail.com>
2024-04-06 11:55:29 +0200

    Animate the projects section heading

--- components/projects.tsx
+++ components/projects.tsx
@@ -5,6 +5,7 @@ import PageHeading from "./page-heading";
 import { projectData } from "@/lib/data";
 import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
 
 export default function Projects() {
   const { ref } = useSectionInView("Projects", 0.2);
@@ -16,11 +17,28 @@ export default function Projects() {
       ref={ref}
     >
       <section className="mt-16 text-center px-4 leading-8 max-w-[80rem]">
-        <div className="flex items-center py-4 mb-8">
+        <motion.div
+          initial={{
+            opacity: 0,
+            scale: 0,
+          }}
+          whileInView={{
+            opacity: 1,
+            scale: 1,
+          }}
+          viewport={{
+            once: true,
+            margin: "-100px",
+          }}
+          transition={{
+            duration: 0.5,
+          }}
+          className="flex items-center py-4 mb-8"
+        >
           <div className="md:hidden flex-grow h-px bg-slate-950 opacity-40 dark:bg-cyan-50"></div>
           <PageHeading>my projects</PageHeading>
           <div className="md:hidden flex-grow h-px bg-slate-950 opacity-40 dark:bg-cyan-50"></div>
-        </div>
+        </motion.div>
 
         {projectData.map((project, index) => (
           <React.Fragment key={index}>
