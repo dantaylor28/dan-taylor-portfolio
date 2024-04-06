a1b77791cb6023c4661aebda31f6281861cac886
Dan <dan.taylor1493@gmail.com>
2024-04-06 11:57:27 +0200

    Animate the experience section heading

--- components/experience.tsx
+++ components/experience.tsx
@@ -7,6 +7,7 @@ import "react-vertical-timeline-component/style.min.css";
 import PageHeading from "./page-heading";
 import { useSectionInView } from "@/lib/hooks";
 import ExperienceElement from "./experience-element";
+import { motion } from "framer-motion";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience", 0.2);
@@ -16,11 +17,28 @@ export default function Experience() {
       ref={ref}
       className="scroll-mt-28 text-center mb-24"
     >
-      <div className="flex items-center p-4">
+      <motion.div
+        initial={{
+          opacity: 0,
+          scale: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          scale: 1,
+        }}
+        viewport={{
+          once: true,
+          margin: "-100px",
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="flex items-center p-4"
+      >
         <div className="md:hidden flex-grow h-px bg-slate-950 opacity-40 dark:bg-cyan-50"></div>
         <PageHeading>my experience</PageHeading>
         <div className="md:hidden flex-grow h-px bg-slate-950 opacity-40 dark:bg-cyan-50"></div>
-      </div>
+      </motion.div>
       <VerticalTimeline lineColor="" className="!mt-20">
         {experienceData.map((item, index) => (
           <ExperienceElement key={index} item={item} />
