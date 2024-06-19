e999ba18d185375e394672c90d0168f04cea17eb
Dan <dan.taylor1493@gmail.com>
2024-06-19 14:21:13 +0200

    Add info to footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='text-center'>
-        <small>&copy; 2024. All rights reserved.</small>
+    <footer className='text-center bg-cyan-800 text-white/90 pb-5 dark:bg-cyan-950'>
+        <small>&copy; 2024: https://www.dantaylor.social. All rights reserved</small>
+        <div>
+            <small>Developed using Next.JS, Typescript & TailwindCSS</small>
+        </div>
     </footer>
   )
 }
